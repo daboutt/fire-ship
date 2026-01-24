f04307d50fcb4aea262dbba08184ec86211f4eb3
daboutt <baotran2115@gmail.com>
2026-01-24 13:30:57 +0700

    feat: basic UI

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <h2>Fire Ship Battle</h2>
+      <div className='lobby'>
+        <p className='lobby-hint'>Start a new game and share the code</p>
+        <div className='lobby-section'>
+          <button>New Game</button>
+        </div>
+        <div className='lobby-divider'>
+          <span>OR</span>
+        </div>
+        <p className='lobby-hint'>Enter the code shared by your friend</p>
+
+        <div className='lobby-section'>
+          <input className='lobby-input-code' type='text' placeholder='code' />
+          <button>Join Game</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
